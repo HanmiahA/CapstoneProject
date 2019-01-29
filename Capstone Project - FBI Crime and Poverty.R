2118f49a362a4f554cf3f9d8116b5e2f6782dbc2
HanmiahA <HanmiahA@users.noreply.github.com>
2019-01-28 19:43:44 -0500

    region column added to tables and code amended

--- Capstone Project - FBI Crime and Poverty.R
+++ Capstone Project - FBI Crime and Poverty.R
@@ -4,23 +4,28 @@ library(tidyverse)
 FBICrime <- read_csv("FBI Crime Data - State and Region (Original).csv")
 
 #Removed extra, unneeded rows and empty columns
+#Removed regions from Area so that the data only included the 50 states plus District of Columbia
 FBICrime <- FBICrime %>% fill(Area) %>% filter(Year != "Percent change", Year != "2016", !is.na(Year)) %>% 
   filter(!(Area %in% c("United States Total5, 6", "Puerto Rico", "Pacific", "Mountain", "West", "West South Central", "East South Central", "South Atlantic5,6", "South5,6", "West North Central", "East North Central", "Midwest", "Middle Atlantic", "Northeast", "New England"))) %>% 
   arrange(Area)
 
-#Removed regions from Area so that the data only included the 50 states plus District of Columbia
-#Arranged the data frame in alphabetical order by state and replaced with abbreviations of the states (including District of Columbia as DC)
-abbreviations <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
-FBICrime$Area <- abbreviations
 
 #Isolated the crime rates by removing the columns containing the number of offenses and renamed column names
 column_names <- make.names(c("State", "Population", "Violent Crime", "Murder", "Rape", "Robbery", "Aggravated Assault", "Property Crime", "Burglary", "Larceny", "Motor Vehicle Theft"))
 FBICrime <- FBICrime %>% select(Area, Population2, starts_with("X"), -X22, -X23, -X24) %>% setNames(column_names)
 
+FBICrime$State[FBICrime$State %in% "District of Columbia5"] <- c("District of Columbia")
+FBICrime$State[FBICrime$State %in% "North Carolina6"] <- c("North Carolina")
+
 #Gathered the crimes into one column and rate into another
 FBICrime <- gather(FBICrime, "Offense", "Rate", -State, -Population)
 FBICrime$Rate <- gsub(",", "", FBICrime$Rate) %>% as.numeric(FBICrime$Rate)
 FBICrime$Population <- gsub(",", "", FBICrime$Population) %>% as.numeric(FBICrime$Population)
+FBICrime <- FBICrime %>% group_by(Offense) %>% mutate(region = tolower(State))
+
+#Arranged the data frame in alphabetical order by state and replaced with abbreviations of the states (including District of Columbia as DC)
+abbreviations <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
+FBICrime$State <- abbreviations
 
 #Filtered out Violent Crime and Property Crime into a separate data frame and removed Violent Crime and Property Crime
 #from the original crime data frame
@@ -28,6 +33,8 @@ Violent_Crime <- filter(FBICrime, Offense == c("Violent.Crime"))
 Property_Crime <- filter(FBICrime, Offense == c("Property.Crime"))
 FBICrime <- FBICrime %>% filter(Offense != c("Violent.Crime")) %>% filter(Offense != c("Property.Crime"))
 FBICrime2 <- bind_rows(Violent_Crime, Property_Crime) %>% arrange(State)
+
+
 
 #Saved new data frame to CSV file
 write.csv(FBICrime2, file = "Violent&PropertyCrime.csv")
@@ -60,5 +67,3 @@ CrimePoverty <- left_join(AvgStateRate, Poverty, by = "State")
 is.num <- sapply(CrimePoverty, is.numeric)
 CrimePoverty[is.num] <- lapply(CrimePoverty[is.num], round, 1)
 write.csv(CrimePoverty, file = "Crime&PovertyByState.csv")
-
-
